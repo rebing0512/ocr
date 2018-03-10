62591ab93c1a39c5127d88839341d103db4fa855
wangjian <wangjian@mbcore.com>
2018-03-10 16:11:04 +0800

    commit

--- src/Http/Libraries/Helper.php
+++ src/Http/Libraries/Helper.php
@@ -13,9 +13,9 @@ class Helper
      * OCR CONFIG 文字识别【百度云】
      */
     function aipOcr(){
-        $APP_ID = config('ocr.id'); //YOUR APP_ID
-        $API_KEY = config('ocr.ak'); //YOUR API_KEY
-        $SECRET_KEY = config('ocr.sk'); //YOUR SECRET_KEY
+        $APP_ID = config('rebing0512_ocr.id'); //YOUR APP_ID
+        $API_KEY = config('rebing0512_ocr.ak'); //YOUR API_KEY
+        $SECRET_KEY = config('rebing0512_ocr.sk'); //YOUR SECRET_KEY
         $aipOcr = new AipOcr($APP_ID, $API_KEY, $SECRET_KEY);
         return $aipOcr;
     }
