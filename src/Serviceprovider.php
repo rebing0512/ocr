72015f65065d8182f373799e6bb61e19378f4906
wangjian <wangjian@mbcore.com>
2018-03-10 10:37:55 +0800

    commit

--- src/Serviceprovider.php
+++ src/Serviceprovider.php
@@ -25,16 +25,16 @@ class ServiceProvider extends BaseServiceProvider{
         $this->loadViewsFrom(realpath(__DIR__.'/resources/views'), 'rebing0512.ocr');  //package::view
 
         // this  for views
-        $this->publishes([
-            __DIR__.'/resources/views' => resource_path('views/rebing0512/ocr'),
-        ], 'views');
+//        $this->publishes([
+//            __DIR__.'/resources/views' => resource_path('views/rebing0512/ocr'),
+//        ], 'views');
 		$this->setupRoutes($this->app->router);
 
 
 		// this  for conig
-        $this->publishes([
-            __DIR__.'/config/rebing0512_ocr.php' => config_path('rebing0512_ocr.php'),
-        ]);
+//        $this->publishes([
+//            __DIR__.'/config/rebing0512_ocr.php' => config_path('rebing0512_ocr.php'),
+//        ]);
 
 //        // 注册 Artisan 命令
 //        if ($this->app->runningInConsole()) {
@@ -51,26 +51,26 @@ class ServiceProvider extends BaseServiceProvider{
 	 * @param  \Illuminate\Routing\Router  $router
 	 * @return void
 	 */
-	public function setupRoutes(Router $router)
-	{
-		$router->group(['namespace' => 'Rebing0512\OCR\Controllers'], function($router)
-        {
-            require __DIR__.'/routes/routes.php';
-        });
-
-	}
+//	public function setupRoutes(Router $router)
+//	{
+//		$router->group(['namespace' => 'Rebing0512\OCR\Controllers'], function($router)
+//        {
+//            require __DIR__.'/routes/routes.php';
+//        });
+//
+//	}
 
 
     /**
      * 注册配置文件
      *
      */
-	public function register()
-	{
-		config([
-		    'config/rebing0512_ocr.php',
-		]);
-	}
+//	public function register()
+//	{
+//		config([
+//		    'config/rebing0512_ocr.php',
+//		]);
+//	}
 
 
 }
